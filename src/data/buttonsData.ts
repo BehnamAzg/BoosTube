16f7d370f24e0a4bb48b84af931ab297a0b6745a
Behnam <110087586+BehnamAzg@users.noreply.github.com>
2026-07-07 14:57:04 +0330

    feat: add buttons data

---
+++ src/data/buttonsData.ts
@@ -0,0 +1,16 @@
+const buttonsData = [
+  {
+    id: 1,
+    title: 'Add "Full Theater" Button',
+    active: true,
+    action: "theater",
+  },
+  {
+    id: 2,
+    title: 'Add "Screenshot" Button',
+    active: false,
+    action: "screenshot",
+  },
+];
+
+export default buttonsData;
