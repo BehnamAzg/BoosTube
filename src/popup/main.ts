49508248f7f837c3410a89ec96d68a71a3ee107a
Behnam <110087586+BehnamAzg@users.noreply.github.com>
2026-07-07 14:57:27 +0330

    feat: import components

--- src/popup/main.ts
+++ src/popup/main.ts
@@ -1,28 +1,10 @@
-import crxLogo from '@/assets/crx.svg'
-import tsLogo from '@/assets/ts.svg'
-import viteLogo from '@/assets/vite.svg'
-import { setupCounter } from './counter.ts'
-import './style.css'
+import Header from "../components/Header";
+import ToggleList from "../components/ToggleList";
+import "./style.css";
 
-document.querySelector('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${tsLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <a href="https://crxjs.dev/vite-plugin" target="_blank">
-      <img src="${crxLogo}" class="logo crx" alt="CRXJS logo" />
-    </a>
-    <h1>Hello CRXJS!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the CRXJS logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter')!)
+document.querySelector("#app")!.innerHTML = `
+  <main class="w-full h-auto min-h-0 text-sm font-medium flex-1 overflow-y-auto p-4 pt-1 ">
+    ${Header()}
+    ${ToggleList()}
+  </main>
+`;
