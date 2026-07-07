5733f9ec171215476661ce8be4a54698b6c0be87
Behnam <110087586+BehnamAzg@users.noreply.github.com>
2026-07-07 08:19:28 +0330

    Replace project with new version

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
+import path from 'node:path'
+import { crx } from '@crxjs/vite-plugin'
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import zip from 'vite-plugin-zip-pack'
+import manifest from './manifest.config.js'
+import { name, version } from './package.json'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': `${path.resolve(__dirname, 'src')}`,
+    },
+  },
   plugins: [
-    tailwindcss(),
+    crx({ manifest }),
+    zip({ outDir: 'release', outFileName: `crx-${name}-${version}.zip` }),
   ],
+  server: {
+    cors: {
+      origin: [
+        /chrome-extension:\/\//,
+      ],
+    },
+  },
 })
